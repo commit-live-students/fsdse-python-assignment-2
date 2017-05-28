82e43387817437cc4b83ba58328bcc938cfb2278
shunya-mandal <100ameya@gmail.com>
2017-05-28 10:33:07 +0000

    Done

--- pair_sum.py
+++ pair_sum.py
@@ -2,10 +2,12 @@ def pairSum(_list, n):
     if len(_list) < 2:
         return
     integer_pairs = []
-    pair_sum = 0
-    for i in _list:
-        for j in _list[1:]:
-            pair_sum = i + j
+    for i in range(len(_list)):
+        for j in range(i+1, len(_list)):
+            pair_sum = _list[i] + _list[j]
             if pair_sum == n:
-                integer_pairs.append([i,j])
+                integer_pairs.append([_list[i] , _list[j]])
     return integer_pairs
+
+
+print(pairSum([4, 3, 1, 6, 2, 0, 2],4))
