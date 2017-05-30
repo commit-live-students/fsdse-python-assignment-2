e43059db9eb9e81144a84c65913596d359bf101d
Rohan2015 <rohandamodar@outlook.com>
2017-05-30 00:29:49 +0000

    Done

--- pair_sum.py
+++ pair_sum.py
@@ -1,11 +1,10 @@
 def pairSum(x,y):
-    Sum = 0
+    #Sum = 0
     outputlist = []
 
     if(len(x) > 2):
         for i in range(0,len(x)-1):
-            Sum = x[i]+x[i+1]
-            if Sum == y:
-                outputlist.append((x[i], x[i+1]))
-        
-        return outputlist
+            for j in range(i+1,len(x)-1):
+                if x[i] + x[j] == y:
+                    outputlist.append((x[i], x[j]))
+    return outputlist
